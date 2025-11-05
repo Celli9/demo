18661035d3fd209db49fe92a6fe137877455c32a
Alex Botticelli <ajb419@georgetown.edu>
2025-11-05 11:38:41 -0500

    Update rps.py

--- app/rps.py
+++ app/rps.py
@@ -1,24 +1,8 @@
 
 import random
-# Ask the user for an input
-user_choice = input("Please choose rock, paper, or scissors")
+
 valid_choices = ['ROCK','PAPER','SCISSORS']
-print(f"You chose {user_choice}")
-# Validate
-user_choice = user_choice.upper()
 
-if user_choice not in valid_choices:
-    print("OOPS INVALID INPUT, PLEASE TRY AGAIN")
-    quit()
-
-# Generate random computer choice
-
-computer_choice = random.choice(valid_choices)
-print(f"Computer Choice: {computer_choice}")
-
-#Determine the winner
-u = user_choice
-c = computer_choice
 def determine_winner(u, c):
     if u == "rock" and c == "rock":
         return "TIE GAME"
@@ -27,20 +11,47 @@ def determine_winner(u, c):
     elif u == "rock" and c == "scissors":
         return "USER WINS"
     elif u == "paper" and c == "rock":
-        return "COMPUTER WINS" # OOPS
+        return "USER WINS" # OOPS
     elif u == "paper" and c == "paper":
         return "TIE GAME"
     elif u == "paper" and c == "scissors":
-        return "USER WINS" # OOPS
+        return "COMPUTER WINS" # OOPS
     elif u == "scissors" and c == "rock":
         return "COMPUTER WINS"
     elif u == "scissors" and c == "paper":
         return "USER WINS"
     elif u == "scissors" and c == "scissors":
         return "TIE GAME"
+    
 
-result = determine_winner(u,c)
-print(result)
+
+if __name__ == "__main__":
+
+    # only run the code below if we are running the script from the CLI, but not if we are just trying to import stuff from the file
+
+    # Ask the user for an input
+    user_choice = input("Please choose rock, paper, or scissors")
+
+    print(f"You chose {user_choice}")
+    # Validate
+    user_choice = user_choice.upper()
+
+    if user_choice not in valid_choices:
+        print("OOPS INVALID INPUT, PLEASE TRY AGAIN")
+        quit()
+
+    # Generate random computer choice
+
+    computer_choice = random.choice(valid_choices)
+    print(f"Computer Choice: {computer_choice}")
+
+    #Determine the winner
+    u = user_choice
+    c = computer_choice
+
+
+    result = determine_winner(u,c)
+    print(result)
 
 # TO test code, need to use assert, and capture the code into a function (refactor the code -- reorg it w/o changing the logic)
 
